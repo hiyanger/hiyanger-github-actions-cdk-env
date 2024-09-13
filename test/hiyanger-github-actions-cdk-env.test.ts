956fff6a2c2bd9cc9b85d76f7d94b1ba7ddcc96c
j-hiyama <j-hiyama@iret.co.jp>
2024-09-13 19:06:23 +0900

    test

---
+++ test/hiyanger-github-actions-cdk-env.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HiyangerGithubActionsCdkEnv from '../lib/hiyanger-github-actions-cdk-env-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hiyanger-github-actions-cdk-env-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HiyangerGithubActionsCdkEnv.HiyangerGithubActionsCdkEnvStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
